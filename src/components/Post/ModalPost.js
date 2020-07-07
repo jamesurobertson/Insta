c27a07118987f248288f61814d9448f919e5dc8d
ajpierskalla3 <ajpierskalla3@gmail.com>
2020-07-07 18:47:00 -0500

    post and media content changes

---
+++ src/components/Post/ModalPost.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+const ModalStyle = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .button-post{
+        height: 48px;
+        background: white;
+        border: none;
+        border-bottom: 1px solid lightgray;
+        outline: none;
+    }
+
+    #button-unfollow{
+        color: red;
+        font-weight: 600;
+    }
+    #button-cancel{
+        border: none;
+    }
+
+    button:active{
+        background: lightgrey;
+        outline: none;
+    }
+
+}
+   `
+
+const ModalPost = () => {
+    return (
+        <ModalStyle>
+            <button id='button-unfollow' className='button-post'>Unfollow</button>
+            <button className='button-post'>Go To Post</button>
+            <button className='button-post'>Copy Link</button>
+            <button id='button-cancel' className='button-post'>Cancel</button>
+        </ModalStyle>
+    )
+}
+
+export default ModalPost;
--- src/components/Post/PhotoImagePost.js
+++ src/components/Post/PhotoImagePost.js
@@ -14,7 +14,7 @@ const PhotoImagePost = () => {
             <img
                 className="post-header-image"
                 src="https://instagram.ffcm1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/83700920_480186686267142_2503895191161667584_n.jpg?_nc_ht=instagram.ffcm1-2.fna.fbcdn.net&_nc_ohc=LsIa8jPnOeEAX_QYVS3&oh=09778b07f671ce0b13ff919809ee5067&oe=5F2E8038"
-                alt="corner photo" />
+                alt="corner img" />
         </PostWrapper>
     );
 };
