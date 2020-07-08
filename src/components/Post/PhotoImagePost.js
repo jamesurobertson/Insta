8a9239da1fe61ea2021f6e89f55ca7f9d05867a9
Zachary Henderson <zachary.henderson114@gmail.com>
2020-07-07 19:16:25 -0500

    fixed redundancy issue

--- src/components/Post/PhotoImagePost.js
+++ src/components/Post/PhotoImagePost.js
@@ -14,15 +14,9 @@ const PhotoImagePost = () => {
             <img
                 className="post-header-image"
                 src="https://instagram.ffcm1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/83700920_480186686267142_2503895191161667584_n.jpg?_nc_ht=instagram.ffcm1-2.fna.fbcdn.net&_nc_ohc=LsIa8jPnOeEAX_QYVS3&oh=09778b07f671ce0b13ff919809ee5067&oe=5F2E8038"
-<<<<<<< Updated upstream
-                alt="corner photo" />
-=======
-<<<<<<< Updated upstream
-                alt="corner img" />
-=======
-                alt="corner" />
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
+                alt="corner " />
+
         </PostWrapper>
     );
 };
